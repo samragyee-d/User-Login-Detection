00b70605532b96ccb56bf6cdba095ad5b25ef27c
Samragyee Dhakal <samragyeedhakal@gmail.com>
2025-06-26 20:31:29 -0400

    super basic for loop stuff

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from sklearn.model_selection import train_test_split
 
 
 df = pd.read_csv('rba-dataset.csv', nrows=1000)
+
+column_a = df['User ID']
+
+for value in column_a:
+    timestamps = df['Login Timestamp']
+    IPAddress = df['IP Address']
+    print(timestamps, IPAddress)
