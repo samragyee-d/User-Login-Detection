6895e102501373a6ab351b715563cfa85fba33ea
Samragyee Dhakal <samragyeedhakal@gmail.com>
2025-06-26 21:15:33 -0400

    Fixed! Program now can differentiate between public and private IPs

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ df = pd.read_csv('rba-dataset.csv', nrows=1000)
 #defining main column
 column_a = df['User ID']
 
-
-
 #setting up IP conversion variables:
 classA = '10.0.0.0/8'
 classB = '172.16.0.0/12'
@@ -22,14 +20,26 @@ networkC = ipaddress.IPv4Network(classC)
 
 
 #iterating throw user logs
+for i, row in df.iterrows():
+    ip_str = row['IP Address']
+    try:
+        ip_obj = ipaddress.ip_address(ip_str)
+        if ip_obj in networkA or ip_obj in networkB or ip_obj in networkC:
+            print(f"{ip_str} is private")
+        else:
+            print(f"{ip_str} is public")
+    except ValueError:
+        print(f"{ip_str} is invalid")
+
+'''
 for value in column_a:
     timestamps = df['Login Timestamp']
     IPAddress = df['IP Address']
     IPAddress = ipaddress.ip_address(IPAddress)
-    if IPAddress in networkA or IPAddress in networkA or IPAddress in networkA:
+    if IPAddress in networkA or IPAddress in networkA or IPAddress in networkC:
         print(f"{IPAddress} is in public")
 else:
     print(f"{IPAddress} is private")
     
     #print(timestamps, IPAddress)
-    
+'''
