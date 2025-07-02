3e3eca09ed34b3bfb10a52936abbca36e4b06bd8
Samragyee Dhakal <samragyeedhakal@gmail.com>
2025-07-02 13:49:26 -0400

    deleted test

--- geotest.py
+++
@@ -1,8 +0,0 @@
-from countryinfo import CountryInfo
-
-country = CountryInfo('United States')
-info = country.info()
-
-# Latitude and longitude are in the 'latlng' key
-latlng = info.get('latlng', [None, None])
-print(latlng)  # Output: [latitude, longitude]
